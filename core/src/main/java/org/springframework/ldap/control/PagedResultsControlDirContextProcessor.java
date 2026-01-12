701f8df7206272072d3afbd5c559af0f063ca392
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-01-12 14:43:03 -0700

    Add PagedResultsControlDirContextProcessor
    This commit adds a simplified DirContextProcessor for
    paging. This new DirContextProcessor does not use
    reflection, clarifying invariants at compile time and
    making it better suited for use in AOT-based
    applications.

---
+++ core/src/main/java/org/springframework/ldap/control/PagedResultsControlDirContextProcessor.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2002-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ldap.control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.ldap.Control;
+import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.PagedResultsControl;
+import javax.naming.ldap.PagedResultsResponseControl;
+
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.ldap.core.DirContextProcessor;
+
+/**
+ * @since 4.1
+ */
+public final class PagedResultsControlDirContextProcessor implements DirContextProcessor {
+
+	private Logger log = LoggerFactory.getLogger(PagedResultsControlDirContextProcessor.class);
+
+	Request request;
+
+	private @Nullable Response response;
+
+	private Consumer<DirContext> noPagedResultsResponseHandler = (ctx) -> {
+		this.log.debug("Failed to find PagedResultsResponseControl in response");
+	};
+
+	public PagedResultsControlDirContextProcessor(Request request) {
+		this.request = request;
+	}
+
+	@Override
+	public void preProcess(DirContext ctx) throws NamingException {
+		if (!(ctx instanceof LdapContext ldap)) {
+			throw new IllegalArgumentException("ctx must be of type LdapContext");
+		}
+		Control[] controls = ldap.getRequestControls();
+		if (controls == null) {
+			ldap.setRequestControls(new Control[] { this.request.delegate });
+			return;
+		}
+		List<Control> updated = new ArrayList<>(Arrays.asList(controls));
+		for (Control control : controls) {
+			if (!(control instanceof PagedResultsControl)) {
+				updated.add(control);
+			}
+		}
+		updated.add(this.request.delegate);
+		ldap.setRequestControls(updated.toArray(Control[]::new));
+	}
+
+	@Override
+	public void postProcess(DirContext ctx) throws NamingException {
+		if (!(ctx instanceof LdapContext ldap)) {
+			throw new IllegalArgumentException("ctx must be of type LdapContext");
+		}
+		Control[] responseControls = ldap.getResponseControls();
+		if (responseControls.length == 0) {
+			this.noPagedResultsResponseHandler.accept(ctx);
+			return;
+		}
+		for (Control responseControl : responseControls) {
+			if (responseControl instanceof PagedResultsResponseControl results) {
+				this.request = new Request(this.request, results);
+				this.response = new Response(results);
+				return;
+			}
+		}
+		this.noPagedResultsResponseHandler.accept(ctx);
+	}
+
+	public @Nullable Response getResponse() {
+		return this.response;
+	}
+
+	public static final class Request {
+		final javax.naming.ldap.PagedResultsControl delegate;
+
+		private final int pageSize;
+
+		private final byte @Nullable [] cookie;
+
+		public Request(int pageSize) {
+			this(pageSize, null, true);
+		}
+
+		public Request(int pageSize, byte @Nullable [] cookie, boolean criticality) {
+			try {
+				this.delegate = new PagedResultsControl(pageSize, cookie, criticality);
+			} catch (IOException ex) {
+				throw new IllegalArgumentException(ex);
+			}
+			this.pageSize = pageSize;
+			this.cookie = cookie;
+		}
+
+		Request(Request request, PagedResultsResponseControl response) {
+			this(request.getPageSize(), response.getCookie(), request.isCritical());
+		}
+
+		public int getPageSize() {
+			return this.pageSize;
+		}
+
+		public byte @Nullable [] getCookie() {
+			return this.cookie;
+		}
+
+		public boolean isCritical() {
+			return this.delegate.isCritical();
+		}
+	}
+
+	public static final class Response {
+		private final PagedResultsResponseControl response;
+
+		public Response(PagedResultsResponseControl response) {
+			this.response = response;
+		}
+
+		public int getResultSize() {
+			return this.response.getResultSize();
+		}
+
+		public byte @Nullable [] getCookie() {
+			return this.response.getCookie();
+		}
+
+		public boolean hasMore() {
+			return this.getCookie() != null;
+		}
+	}
+}
---
+++ core/src/test/java/org/springframework/ldap/control/PagedResultsControlDirContextProcessorTests.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2006-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ldap.control;
+
+import javax.naming.ldap.Control;
+import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.PagedResultsControl;
+import javax.naming.ldap.PagedResultsResponseControl;
+import javax.naming.ldap.SortControl;
+
+import com.sun.jndi.ldap.ctl.DirSyncResponseControl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import org.springframework.ldap.control.PagedResultsControlDirContextProcessor.Request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Josh Cummings
+ */
+class PagedResultsControlDirContextProcessorTests {
+
+	private LdapContext ldapContextMock;
+
+	private PagedResultsControlDirContextProcessor tested;
+
+	@BeforeEach
+	void setUp() {
+		this.tested = new PagedResultsControlDirContextProcessor(new Request(20));
+		this.ldapContextMock = mock(LdapContext.class);
+	}
+
+	@Test
+	void preProcessWhenPagedRequestControlsThenReplaces() throws Exception {
+		Control[] request = new Control[] { new PagedResultsControl(15, false) };
+		given(this.ldapContextMock.getRequestControls()).willReturn(request);
+		this.tested.preProcess(this.ldapContextMock);
+		ArgumentCaptor<Control[]> captor = ArgumentCaptor.forClass(Control[].class);
+		verify(this.ldapContextMock).setRequestControls(captor.capture());
+		Control[] controls = captor.getValue();
+		assertThat(controls).hasSize(1);
+		assertThat(controls[0]).isInstanceOf(PagedResultsControl.class);
+		assertThat(this.tested.request.delegate).isSameAs(controls[0]);
+	}
+
+	@Test
+	void preProcessWhenNoRequestControlsThenAdds() throws Exception {
+		this.tested.preProcess(this.ldapContextMock);
+		ArgumentCaptor<Control[]> captor = ArgumentCaptor.forClass(Control[].class);
+		verify(this.ldapContextMock).setRequestControls(captor.capture());
+		Control[] controls = captor.getValue();
+		assertThat(controls).hasExactlyElementsOfTypes(PagedResultsControl.class);
+	}
+
+	@Test
+	void preProcessWhenRequestControlsThenAdds() throws Exception {
+		Control[] request = new Control[] { new SortControl("key", true) };
+		given(this.ldapContextMock.getRequestControls()).willReturn(request);
+		this.tested.preProcess(this.ldapContextMock);
+		ArgumentCaptor<Control[]> captor = ArgumentCaptor.forClass(Control[].class);
+		verify(this.ldapContextMock).setRequestControls(captor.capture());
+		Control[] controls = captor.getValue();
+		assertThat(controls).hasExactlyElementsOfTypes(SortControl.class, PagedResultsControl.class);
+		assertThat(controls[1]).isInstanceOf(PagedResultsControl.class);
+		assertThat(this.tested.request.delegate).isSameAs(controls[1]);
+	}
+
+	@Test
+	void postProcessWhenNoResponseThenIgnores() throws Exception {
+		given(this.ldapContextMock.getResponseControls()).willReturn(null);
+
+		this.tested.postProcess(this.ldapContextMock);
+
+		assertThat(this.tested.getResponse()).isNull();
+		assertThat(this.tested.request.getPageSize()).isEqualTo(20);
+		assertThat(this.tested.request.getCookie()).isNull();
+	}
+
+	@Test
+	void postProcessWhenResponsesThenUsesPagedResultsResponseControl() throws Exception {
+		PagedResultsResponseControl response = spy(new PagedResultsResponseControl("id", true, new byte[0]));
+		given(response.getResultSize()).willReturn(15);
+		given(response.getCookie()).willReturn(new byte[] { 1, 2, 3 });
+		given(this.ldapContextMock.getResponseControls()).willReturn(new Control[] {
+			response, new DirSyncResponseControl("dummy", true, null) });
+
+		this.tested.postProcess(this.ldapContextMock);
+
+		assertThat(this.tested.getResponse()).isNotNull();
+		assertThat(this.tested.getResponse().hasMore()).isTrue();
+		assertThat(this.tested.getResponse().getResultSize()).isEqualTo(15);
+		assertThat(this.tested.getResponse().getCookie()).isEqualTo(new byte[] { 1, 2, 3 });
+	}
+
+}
